419908679957c2e4d416c87cc9add6cd6e34d046
fahadali06 <senser90@hotmail.com>
2017-08-07 11:25:44 +0500

    Fetch record from database

---
+++ include/conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'php-mysql';
+
+$conn = mysqli_connect($host, $user, $password, $database);
+//if ($conn) {
+//    echo "success";
+//}
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "<h4>Hello World</h4>";
+include 'include/conn.php';
+
+$sql = 'SELECT * FROM users';
+$query = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_assoc($query)) {
+    echo 'Name: '.$row['name'].', Age: '.$row['age'].', Sex: '.$row['sex'].'<br>';
+}
